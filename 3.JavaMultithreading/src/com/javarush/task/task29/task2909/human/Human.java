4dd728c20ce47b0379ece5bf601b27cf274a445e
frenchman <kujzub@gmail.com>
2017-02-19 04:28:39 +0200

    add task29.task2909

--- 3.JavaMultithreading/src/com/javarush/task/task29/task2909/human/Human.java
+++ 3.JavaMultithreading/src/com/javarush/task/task29/task2909/human/Human.java
@@ -1,12 +1,16 @@
 package com.javarush.task.task29.task2909.human;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class Human {
     public static int nextId = 0;
+    private List<Human> children = new ArrayList<>();
     private int id;
     protected int age;
     protected String name;
     protected int course;
-
     protected int[] size;
 
     protected boolean isSoldier;
@@ -69,5 +73,17 @@ public class Human {
 
     public void printSize() {
         System.out.println("Рост: " + size[0] + " Вес: " + size[1]);
+    }
+
+    public List<Human> getChildren() {
+        return Collections.unmodifiableList(children);
+    }
+
+    public void addChild(Human human) {
+        children.add(human);
+    }
+
+    public void removeChild(Human human) {
+        children.remove(human);
     }
 }
