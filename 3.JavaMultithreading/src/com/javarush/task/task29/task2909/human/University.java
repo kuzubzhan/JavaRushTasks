d8a25b67aa4ede4d90aab657030e6935d093df43
frenchman <kujzub@gmail.com>
2017-02-22 01:14:52 +0200

    add task29.task2909

--- 3.JavaMultithreading/src/com/javarush/task/task29/task2909/human/University.java
+++ 3.JavaMultithreading/src/com/javarush/task/task29/task2909/human/University.java
@@ -1,7 +1,6 @@
 package com.javarush.task.task29.task2909.human;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class University {
     private String name;
@@ -14,18 +13,43 @@ public class University {
         students = new ArrayList<>();
     }
 
-    public Student getStudentWithAverageGrade() {
-        //TODO:
+    public Student getStudentWithAverageGrade(double averageGrade) {
+        if (!students.isEmpty()) {
+            for (Student student : students) {
+                if (student.getAverageGrade() == averageGrade)
+                    return student;
+            }
+        }
         return null;
     }
 
-    public Student getStudentWithMaxAverageGrade(double averageGrade) {
-        //TODO:
+    public Student getStudentWithMaxAverageGrade() {
+        if (!students.isEmpty()) {
+            Student student = students.get(0);
+            for (Student student1 : students) {
+                if (student1.getAverageGrade() > student.getAverageGrade())
+                    student = student1;
+            }
+            return student;
+        }
         return null;
     }
 
-    public void getStudentWithMinAverageGradeAndExpel() {
-        //TODO:
+    public Student getStudentWithMinAverageGrade() {
+        if (!students.isEmpty()) {
+            Student student = students.get(0);
+            for (Student student1 : students) {
+                if (student1.getAverageGrade() < student.getAverageGrade())
+                    student = student1;
+            }
+            return student;
+        }
+        return null;
+    }
+
+    public void expel(Student student) {
+        if (students.contains(student))
+            students.remove(student);
     }
 
     public List<Student> getStudents() {
