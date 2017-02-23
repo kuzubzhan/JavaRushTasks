84274159fd306a9e6f92998224f93ee001064c4b
frenchman <kujzub@gmail.com>
2017-02-23 02:13:05 +0200

    add task29.task2909

--- 3.JavaMultithreading/src/com/javarush/task/task29/task2909/car/Car.java
+++ 3.JavaMultithreading/src/com/javarush/task/task29/task2909/car/Car.java
@@ -2,7 +2,7 @@ package com.javarush.task.task29.task2909.car;
 
 import java.util.Date;
 
-public class Car {
+public abstract class Car {
     static public final int TRUCK = 0;
     static public final int SEDAN = 1;
     static public final int CABRIOLET = 2;
@@ -40,12 +40,10 @@ public class Car {
     }
 
     public int getNumberOfPassengersCanBeTransferred() {
-        if (!isDriverAvailable())
-            return 0;
-        if (fuel <= 0)
-            return 0;
-
-        return numberOfPassengers;
+        if (canPassengersBeTransferred()) {
+            return numberOfPassengers;
+        }
+        return 0;
     }
 
     public boolean isDriverAvailable() {
@@ -59,10 +57,8 @@ public class Car {
     public void startMoving() {
         if (numberOfPassengers > 0) {
             fastenPassengersBelts();
-            fastenDriverBelt();
-        } else {
-            fastenDriverBelt();
         }
+        fastenDriverBelt();
     }
 
     public void fastenPassengersBelts() {
@@ -71,13 +67,7 @@ public class Car {
     public void fastenDriverBelt() {
     }
 
-    public int getMaxSpeed() {
-        if (type == TRUCK)
-            return 80;
-        if (type == SEDAN)
-            return 120;
-        return 90;
-    }
+    public abstract int getMaxSpeed();
 
     public static Car create(int type, int numberOfPassengers) {
         switch (type) {
@@ -102,5 +92,12 @@ public class Car {
 
     public double getSummerConsumption(int length) {
         return length * summerFuelConsumption;
+    }
+
+    private boolean canPassengersBeTransferred() {
+        if (isDriverAvailable() && fuel > 0)
+            return true;
+        else
+            return false;
     }
 }
